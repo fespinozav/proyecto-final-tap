db53385c0aa43546e8d7d1e44ab45c0e5da1edfc
Osval295 <osval2.delgado.gonzalez@gmail.com>
2025-08-18 22:51:27 +0000

    update base genomes_folders

--- scripts/oparse_metadata.sh
+++ scripts/oparse_metadata.sh
@@ -1,6 +1,6 @@
 # ejemplos de uso:
 # 16 especies
-# python oparse_metadata.py --tabla ../Supplememtary_Table_1.csv --especies "s__Enterobacter hoffmannii, s__Aeromonas veronii, s__Aeromonas caviae, s__Limosilactobacillus fermentum, s__Lacticaseibacillus rhamnosus, s__Burkholderia glumae, s__Rickettsia rickettsii, s__Staphylococcus pseudintermedius, s__Mycobacterium intracellulare, s__Enterobacter hormaechei_B, s__Burkholderia multivorans, s__Clostridium_F botulinum, s__Bifidobacterium longum, s__Escherichia coli, s__Bacillus_A cereus, s__Xylella fastidiosa" --ruta_fasta "/data/T-cnicas-Avanzadas-de-Programaci-n/0. Proyecto/Data_forTAP/*.fna" --salidas "../outputs"
+# python oparse_metadata.py --tabla ../Supplememtary_Table_1.csv --especies "s__Enterobacter hoffmannii, s__Aeromonas veronii, s__Aeromonas caviae, s__Limosilactobacillus fermentum, s__Lacticaseibacillus rhamnosus, s__Burkholderia glumae, s__Rickettsia rickettsii, s__Staphylococcus pseudintermedius, s__Mycobacterium intracellulare, s__Enterobacter hormaechei_B, s__Burkholderia multivorans, s__Clostridium_F botulinum, s__Bifidobacterium longum, s__Escherichia coli, s__Bacillus_A cereus, s__Xylella fastidiosa" --ruta_fasta "../genomes/folder with 3 species/*.fna" --salidas "../outputs"
 
-# 1 especie
-python oparse_metadata.py --tabla ../Supplememtary_Table_1.csv --especies "s__Mycobacterium intracellulare" --ruta_fasta "../genomes/Mycobacterium_intracellulare/*.fna" --salidas "../outputs"
+# 3 especies
+python oparse_metadata.py --tabla ../Supplememtary_Table_1.csv --especies "s__Klebsiella aerogenes, s__Mycobacterium_intracellulare, s__Streptococcus suis" --ruta_fasta "../genomes/folder with 3 species/*.fna" --salidas "../outputs"
